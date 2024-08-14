fb00a9986f3a716e51afe52ca3ec3abbacc7e4f2
Mohamed-Ashraf186 <mohamedashraf28812@gmail.com>
2024-08-14 13:37:05 +0300

    WIP

--- worldwise/src/components/Sidebar.jsx
+++ worldwise/src/components/Sidebar.jsx
@@ -1,15 +1,17 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
-import styles from "./SideBar.module.css";
+import styles from "./Sidebar.module.css";
+
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear} by WorldWise Inc
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc
         </p>
       </footer>
     </div>
