e09b940253fadfd8ee16930d4e01ba1cd06eaa19
Mohamed-Ashraf186 <mohamedashraf28812@gmail.com>
2024-08-16 16:02:22 +0300

    WIP

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -13,7 +13,11 @@ function City() {
   // TEMP DATA
 
   const { id } = useParams();
-  console.log(x);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   const currentCity = {
     cityName: "Lisbon",
     emoji: "🇵🇹",
@@ -23,7 +27,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <h2>
+        Position:{lat}, {lng}
+      </h2>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
