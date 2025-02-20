63b4761035809d88d7e220c88269b19b7d26bf0f
Mohamed-Ashraf186 <mohamedashraf28812@gmail.com>
2025-02-20 16:38:19 +0200

    WIP supabase

---
+++ the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://jmoaiegncsskukvzcely.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptb2FpZWduY3Nza3VrdnpjZWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNDU1ODIsImV4cCI6MjA1NTYyMTU4Mn0.5mzmZFzgq-oWpzcstC65PbCCC-GEgnxB7fHxjRiCnyE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
